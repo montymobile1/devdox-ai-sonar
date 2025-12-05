a8d4b654ef03959f41830a15fcd7329c7cf93681
hayat.bourgi <hayat.bourgi@montyholding.com>
2025-12-05 14:40:47 +0200

    fix issues and refactor code

--- src/devdox_ai_sonar/__init__.py
+++ src/devdox_ai_sonar/__init__.py
@@ -2,25 +2,18 @@
 DevDox AI Sonar - AI-powered Sonar fix issues
 """
 
-
-
 __version__ = "0.0.1"
 __author__ = "Hayat Bourgi"
 __email__ = "hayat.bourgi@montyholding.com"
 
 from .sonar_analyzer import SonarCloudAnalyzer
-from .models import SonarIssue, AnalysisResult, FixSuggestion
 from .fix_validator import FixValidator, ValidationStatus
 from .llm_fixer import LLMFixer
 
 __all__ = [
     "SonarCloudAnalyzer",
-    "SonarIssue",
-    "AnalysisResult",
-    "FixSuggestion",
     "LLMFixer",
     "FixValidator",
     "ValidationStatus",
     "__version__",
 ]
-
