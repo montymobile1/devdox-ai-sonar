12d24d6021fc88b295fa9b74a42e0992233a4e03
hayat.bourgi <hayat.bourgi@montyholding.com>
2026-03-03 13:23:26 +0200

    Bump version

--- src/devdox_ai_sonar/config.py
+++ src/devdox_ai_sonar/config.py
@@ -10,7 +10,7 @@ from pathlib import Path
 class Settings(BaseSettings):
     """Application settings."""
 
-    VERSION: str = "0.0.4"
+    VERSION: str = "0.0.5"
 
     CONFIG_DIR: Path = Field(
         default_factory=lambda: Path.home() / "devdox",
